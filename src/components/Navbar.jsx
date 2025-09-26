39dc1bccd48273e7b30d99cea61b730385a54ff3
Hassaan <60301901@udst.edu.qa>
2025-09-26 21:54:37 +0300

    testts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
 				/>
 				<Avatar style={{ height: "32px", width: "32px" }} />
 			</div>
-			{/* testing number 2 */}
+			{/* testing number 2123123 */}
 		</nav>
 	);
 };
